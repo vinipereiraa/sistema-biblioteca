f5f3e36476ec657219f6c2ac378103cd69b27b47
Vinícius Caetano Pereira <vinicius.pereira252@etec.sp.gov.br>
2023-07-06 15:32:02 -0300

    alteração no estilo

--- estilo/estilo.php
+++ estilo/estilo.php
@@ -6,46 +6,48 @@
     margin: 0;
     padding: 0;
     font-family: Arial, Helvetica, sans-serif;
-    background-color: lightblue;
 }
 
-fieldset{
+body {
+    background-color: powderblue;
+}
+
+.agroup {
+    background-color: honeydew;
+    text-align: center;
     margin-left: 25%;
     margin-right: 25%;
     margin-top: 10%;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    border: 1px solid black;
+    padding: 10px;
+    border-radius: 8px;
+    box-shadow:  3px 3px 3px gray;
+}
+
+#title{
+    font-family: 'Lilita One';
+}
+
+.form {
+    text-align: center;
+}
+
+label {
+    font-size: 15px;
+}
+
+#input {
+    border-radius: 2px;
 }
 
 .inp {
-    font-size: 15px;
-    background-color: lightskyblue;
-    border-radius: 0.1px;  
-    font-family: Arial, Helvetica, sans-serif;  
+    background-color: white;
+    padding: 3px;
+    border-radius: 2px;
 }
 
-.inp:hover{
-    background-color: lightblue;
-    transition: 1s;
-}
-
-#form{
-    margin-top: 20px;
-}
-
-legend {
-    border: 1px solid black;
-    border-radius: 5px;
-    margin-left: 30px;
-    padding: 5px;
-    font-family: 'Lilita One', cursive;
-}
-
-p {
-    font-family: 'Lilita One', cursive;
-    font-size: 25px;
+.inp:hover {
+    transition: .4s;
+    background-color: powderblue;
 }
 
 </style>
--- index.php
+++ index.php
@@ -8,28 +8,29 @@
     <?php include 'estilo/estilo.php'; ?>
 </head>
 <body>
-    <fieldset>
-        <legend id="leg"><p id="tit">Seja bem vindo a Biblioteca Virtual &#128218;</p></legend>
-        <form action="php/bibliotecacontroller.php" method="POST" id="form">
+    <div class="agroup">
+        <h2 id="title">Seja bem vindo a Biblioteca Virtual &#128218;</h2>
+        <br>
+        <form action="php/bibliotecacontroller.php" method="POST" class="form">
         <label for="cod_livro">Código do Livro</label>
-        <input type="text" name="cod_livro"><br><br>
+        <input type="text" name="cod_livro" id="input"><br><br>
         <label for="nome">Nome do Livro</label>
-        <input type="text" name="nome"><br><br>
+        <input type="text" name="nome" id="input"><br><br>
         <label for="autor">Autor do Livro</label>
-        <input type="text" name="autor"><br><br>
+        <input type="text" name="autor" id="input"><br><br>
         <label for="editora">Editora do Livro</label>
-        <input type="text" name="editora"><br><br>
+        <input type="text" name="editora" id="input"><br><br>
         <label for="edicao">Edição do Livro</label>
-        <input type="text" name="edicao"><br><br>
+        <input type="text" name="edicao" id="input"><br><br>
         <label for="num_pgs">Número de Páginas</label>
-        <input type="text" name="num_pgs"><br><br>
+        <input type="text" name="num_pgs" id="input"><br><br>
         <label for="ano_de_public">Ano de Publicação</label>
-        <input type="text" name="ano_de_public"><br><br>
+        <input type="text" name="ano_de_public" id="input"><br><br>
         <input type="submit" value="cadastrar" name="botao" class="inp">
         <input type="submit" value="deletar" name="botao" class="inp">
         <input type="submit" value="atualizar" name="botao" class="inp">
         <input type="submit" value="consultar" name="botao" class="inp">
         </form>
-    </fieldset>
+    </div>
 </body>
 </html>
